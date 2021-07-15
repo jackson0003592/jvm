d0399f6b042804cdde8ddab89185e8735c168a70
0003592 刘东洋 <liudongyang@suiyi.com.cn>
2021-07-15 14:20:03 +0800

    虚拟机栈

---
+++ chapter2/src/main/java/com/atguigu/java1/OperandStackTest.java
@@ -0,0 +1,21 @@
+package com.atguigu.java1;
+
+public class OperandStackTest {
+
+
+    public static void main(String[] args) {
+        byte i = 15;
+        int j = 8;
+        int k = i + j;
+    }
+
+    private void add() {
+        int i1 = 10;
+        i1++;
+
+        int i2 = 10;
+        ++i2;
+    }
+
+
+}
